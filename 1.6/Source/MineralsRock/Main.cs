f60ddd47c857c7e447797c353a86bff4cce3ea91
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2025-08-11 21:40:34 -0700

    extract and rework code from Minerals

---
+++ 1.6/Source/MineralsRock/Main.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+
+namespace MineralsRock
+{
+    class MineralsRockMain : Mod
+    {
+        public static MineralsRockSettings Settings;
+
+        public MineralsRockMain(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<MineralsRockSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Settings.DoWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "MineralsRock";
+        }
+    }
+
+}
