2698c9c41978f91ccc10471c45bf1d7136999b08
Zachary S.L. Foster <zacharyfoster1989@gmail.com>
2025-08-23 13:08:53 -0700

    patch meteorite falls

---
+++ 1.6/Source/MineralsRock/Main.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using MineralsFramework;
+
+namespace MineralsRock
+{
+    public class MineralsRockMain : Mod
+    {
+        public static Harmony harmony;
+
+        public MineralsRockMain(ModContentPack content) : base(content)
+        {
+            harmony = new Harmony("zacharyfoster.MineralsFramework");
+            harmony.PatchAll();
+
+            if (MineralsFrameworkMain.Settings.debugModeEnabled)
+            {
+                Log.Message("MineralsRock: Harmony patches applied");
+            }
+        }
+
+    }
+}
---
+++ 1.6/Source/MineralsRock/harmonyPatching.cs
@@ -0,0 +1,46 @@
+using HarmonyLib;
+using MineralsFramework;
+using MineralsRock;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace MineralsRock
+{
+    [HarmonyPatch(typeof(ThingSetMaker_Meteorite), "Reset")]
+    public static class ThingSetMaker_Meteorite_Reset_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ThingSetMaker_Meteorite __instance)
+        {
+            // Modify the static nonSmoothedMineables list after reset
+            var filteredMineables = (DefDatabase<ThingDef>.AllDefsListForReading.Where((ThingDef x) => x.mineable && !x.building.mineablePreventMeteorite && !x.IsSmoothed &&
+typeof(MineralsFramework.ThingDef_StaticMineral).IsAssignableFrom(x.GetType()))).ToList();
+
+            ThingSetMaker_Meteorite.nonSmoothedMineables = filteredMineables;
+
+            if (MineralsFrameworkMain.Settings.debugModeEnabled)
+            {
+                Log.Message($"Minerals: Replaced meteorite mineables with {filteredMineables.Count} MineralsFramework defs");
+            }
+        }
+    }
+
+
+    [HarmonyPatch(typeof(ThingSetMaker_Meteorite), "FindRandomMineableDef")]
+    public static class ThingSetMaker_Meteorite_FindRandomMineableDef_Patch
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref ThingDef __result)
+        {
+            // Completely replace original logic with simple random selection
+            if (ThingSetMaker_Meteorite.nonSmoothedMineables.Count == 0)
+                return true;
+            
+            __result = ThingSetMaker_Meteorite.nonSmoothedMineables.RandomElement();
+            
+            return false; // Skip original method
+        }
+    }
+}
